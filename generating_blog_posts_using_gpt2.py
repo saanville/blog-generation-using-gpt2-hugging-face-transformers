a182d98bb8a6887434e6c46f0d02e7eb65bd434b
Hassaan Nadeem <hassaan.butt9999@outlook.com>
2021-08-07 17:37:35 +0500

    initial commit

---
+++ generating_blog_posts_using_gpt2.py
@@ -0,0 +1,15 @@
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+model = GPT2LMHeadModel.from_pretrained('gpt2', pad_token_id=tokenizer.eos_token_id)
+
+sentence = "Any input text that you wanna start your blog with"
+
+length = None   #enter the maximum length of the string that you want to generate
+
+input_ids = tokenizer.encode(sentence, return_tensors="pt")
+
+output = model.generate(input_ids, max_length=length, num_beams=5, no_repeat_ngram_size=2, early_stopping=True)
+
+output_text = tokenizer.decode(output[0])
+output_text
